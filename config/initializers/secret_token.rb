49df23bbb5f897f9f22ebeb825db7f65276a597d
Neal Pisenti <npisenti@gmail.com>
2011-10-20 05:17:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookCatalogue::Application.config.secret_token = '5ba4ed562d5d434c2c8e6f5d24ba090a948c2e96e5341b6103291ed46bdede96925f4b6a16a12c4bf456b9bb4dcf47c7fee5ec09b56eddf0ad028b29f54115ea'
