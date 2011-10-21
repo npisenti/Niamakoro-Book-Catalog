16e3a15d478bead2d5abad16642fc20363275bc2
Neal Pisenti <npisenti@gmail.com>
2011-10-21 06:44:29 -0700

    added some styling, a subjects form to book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,15 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
     @book.authors = [Author.new]
+    @book.subjects = [Subject.new]
   end
 
   def create
     @book = Book.create(params[:book])
     @author = Author.find_or_create_by_first_and_last(params[:author])
     @book.authors = [@author]
+    @subject = Subject.find_or_create_by_name(params[:subject])
+    @book.subjects = [@subject]
 
     if @book.save
       redirect_to @book
