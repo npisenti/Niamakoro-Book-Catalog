8296f039c969030e1a5c8455c2c27de42967c8b6
Neal Pisenti <npisenti@gmail.com>
2012-01-06 05:21:32 -0800

    added a pretty_name function to grade model, modified some views

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,5 +1,19 @@
+# coding: utf-8
 class Grade < ActiveRecord::Base
   has_many :class_records
 
   validates_presence_of :year
+
+  default_scope :order => 'year'
+
+
+  def pretty_name
+    if year == 0
+      "Jardin"
+    elsif year == 1
+      "1ère année"
+    else
+      "#{year}ème année"
+    end
+  end
 end
