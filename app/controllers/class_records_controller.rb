d4a403188b4d0c2fc5288eed13ae034b847994f5
Neal Pisenti <npisenti@gmail.com>
2012-01-13 02:27:41 -0800

    added edit for classrecords

--- app/controllers/class_records_controller.rb
+++ app/controllers/class_records_controller.rb
@@ -22,6 +22,25 @@ class ClassRecordsController < ApplicationController
     end
   end
 
+  def edit
+    @class_record = ClassRecord.find(params[:id])
+  end
+
+  def update
+    @class_record = ClassRecord.find(params[:id])
+    data = ClassRecord.fix_params(params)
+
+    @class_record.update_attributes(:arrival => data[:class_record][:arrival],
+                      :departure => data[:class_record][:departure],
+                      :teacher_attendance => data[:class_record][:teacher_attendance],
+                      :attendance => data[:class_record][:attendance],
+                      :notes => data[:class_record][:notes])
+    if @class_record.save
+      redirect_to @class_record
+    else
+      render 'edit'
+    end
+  end
 
   def show
     @class_record = ClassRecord.find(params[:id])
